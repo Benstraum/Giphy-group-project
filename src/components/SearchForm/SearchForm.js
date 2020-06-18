de4978828e52b49b9a06765ab0b36dbc913833f8
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-06-18 12:24:00 -0500

    Action dispatches successfully

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,30 +2,42 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class SearchForm extends Component {
-
   state = {
-    searchInput: ' '
-  }
+    searchInput: " ",
+  };
 
-  handleSearchInput = event => {
+  handleSearchInput = (event) => {
     this.setState({
-      searchInput: event.target.value
-    })
+      searchInput: event.target.value,
+    });
     console.log(this.state);
-  }
+  };
 
-
-
+  setSearch = (event) => {
+    this.props.dispatch({
+      type: "GET_SEARCH",
+      payload: this.state.searchInput,
+    });
+    this.setState({
+      searchInput: " ",
+    });
+  };
 
   render() {
     return (
       <div>
         <span>Find GIFs: </span>
-        <input type="text" placeholder="What's your flavor?" onChange={this.handleSearchInput} />
-        <button type="submit">Search</button>
+        <input
+          type="text"
+          placeholder="What's your flavor?"
+          onChange={this.handleSearchInput}
+        />
+        <button onClick={this.setSearch} type="submit">
+          Search
+        </button>
       </div>
     );
   }
 }
 
-export default (SearchForm);
+export default connect()(SearchForm);
