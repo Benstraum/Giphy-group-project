04b3e5f208b6773ce7d0d4671f87dab1db15525e
ben straumann <benstraum@gmail.com>
2020-06-19 12:25:53 -0500

    format doc

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { HashRouter as Router, Route, Link } from "react-router-dom";
 import Search from "../Search/Search";
-import  FavoritesList  from "../FavoritesList/FavoritesList";
+import FavoritesList from "../FavoritesList/FavoritesList";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import image from '../../images/giphy-logo-1.svg'
@@ -9,35 +9,35 @@ import image from '../../images/giphy-logo-1.svg'
 class Header extends Component {
   render() {
     return <div>
-        <header className="headIt">
-            <div>
-            <Router> <div>
-        <AppBar position="static">
-            <Toolbar>
-          <div className='navBar'>
-          <ol>
-            <li>
-            <img alt="giphy logo svg"src={image}></img>
-            </li>
-            <li >
-              <h3>Group Giphy Project</h3>
-            </li>
-            <li className="link">
-            <Link to="/" >Home/Search</Link>
-            </li>
-            <li className="link">
-            <Link to="/favorites">Fav's</Link>
-            </li>
-          </ol>
+      <header className="headIt">
+        <div>
+          <Router> <div>
+            <AppBar position="static">
+              <Toolbar>
+                <div className='navBar'>
+                  <ol>
+                    <li>
+                      <img alt="giphy logo svg" src={image}></img>
+                    </li>
+                    <li >
+                      <h3>Group Giphy Project</h3>
+                    </li>
+                    <li className="link">
+                      <Link to="/" >Home/Search</Link>
+                    </li>
+                    <li className="link">
+                      <Link to="/favorites">Fav's</Link>
+                    </li>
+                  </ol>
+                </div>
+              </Toolbar>
+            </AppBar>
+            <Route exact path="/" component={Search} />
+            <Route path="/favorites" component={FavoritesList} />
           </div>
-          </Toolbar>
-          </AppBar>
-          <Route exact path="/" component={Search} />
-          <Route path="/favorites" component={FavoritesList} />          
+          </Router>
         </div>
-      </Router>
-            </div>
-        </header>
+      </header>
     </div>;
   }
 }
