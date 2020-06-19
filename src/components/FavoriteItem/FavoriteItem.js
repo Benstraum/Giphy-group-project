b30d786c63c3e7aca3d01c14d02c8e636799ec2c
ben straumann <benstraum@gmail.com>
2020-06-19 10:33:14 -0500

    sweetalert

--- src/components/FavoriteItem/FavoriteItem.js
+++ src/components/FavoriteItem/FavoriteItem.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Swal from 'sweetalert2'
+
 export class FavoriteItem extends Component {
   removeFromFavorites = (id) => {
     console.log("this will be an PUT request");
@@ -11,11 +13,33 @@ export class FavoriteItem extends Component {
 
   deleteGiphy = (id) => {
     console.log("this will be a DELETE request");
-    this.props.dispatch({
-      type: "DELETE_GIPHY",
-      payload: id,
-    });
-    this.props.dispatch({ type: "GET_ALL_FAVORITES" });
+    Swal.fire({
+        title: 'Are you sure?',
+        text: 'You will not be able to recover this imaginary file!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, keep it'
+      }).then((result) => {
+        if (result.value) {
+          Swal.fire(
+            'Deleted!',
+            'That gif was deleted.',
+            'success',
+            this.props.dispatch({
+                type: "DELETE_GIPHY",
+                payload: id,
+              }),
+              this.props.dispatch({ type: "GET_ALL_FAVORITES" }),
+          )
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          Swal.fire(
+            'Cancelled',
+            'Your gif is safe :)',
+            'error'
+          )
+        }
+      })
   }; //end delete
 
   state = {
